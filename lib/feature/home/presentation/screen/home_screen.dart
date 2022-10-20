f0d4ee2893f55c6800cb0624738c834ce48a9915
wahyuanugrah <andri.anugrah@bfi.co.id>
2022-10-20 15:16:31 +0700

    add home page

--- lib/feature/home/presentation/screen/home_screen.dart
+++ lib/feature/home/presentation/screen/home_screen.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_newspaper_app/feature/home/data/home_model.dart';
+import 'package:flutter_newspaper_app/feature/home/data/homeuserdata.dart';
 import 'package:flutter_newspaper_app/feature/home/presentation/bloc/home_bloc/bloc.dart';
 
 class HomeScreen extends StatefulWidget {
-  HomeScreen(this.title);
-  final String title;
+  HomeScreen({this.homeduserdata});
+  // final String? title;
+  Homeduserdata? homeduserdata;
   @override
   State<HomeScreen> createState() => _HomeScreenState();
 }
@@ -24,11 +26,11 @@ class _HomeScreenState extends State<HomeScreen> {
         appBar: AppBar(
           // title: Text(widget.title),
           actions: [
-            Center(child: Text(widget.title)),
+            Center(child: Text("Nesw Sport")),
             SizedBox(
               width: 10,
             ),
-            CircleAvatar(),
+            // CircleAvatar(),
             SizedBox(
               width: 10,
             ),
@@ -70,26 +72,51 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 
   Widget lisnews(HomeModel homeModel) {
-    return Container(
-      width: MediaQuery.of(context).size.width,
-      height: MediaQuery.of(context).size.height,
-      child: ListView.builder(
-        itemCount: homeModel.article?.length,
-        itemBuilder: (context, index) {
-          return Container(
-            margin: EdgeInsets.symmetric(vertical: 5),
-            child: Card(
-              child: ListTile(
-                leading: Image.network(
-                    homeModel.article![index].urlToImage.toString()),
-                title: Text(homeModel.article![index].title.toString()),
-                subtitle:
-                    Text(homeModel.article![index].content!.substring(0, 50)),
-              ),
-            ),
-          );
-        },
-      ),
+    return Column(
+      children: [
+        (widget.homeduserdata?.user == "user")
+            ? Container(
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height * 0.05,
+                color: Colors.red,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    InkWell(
+                      child: Text(
+                        "Login",
+                        style: TextStyle(color: Colors.white, fontSize: 20),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 10,
+                    )
+                  ],
+                ),
+              )
+            : Container(),
+        Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height * 0.85,
+          child: ListView.builder(
+            itemCount: homeModel.article?.length,
+            itemBuilder: (context, index) {
+              return Container(
+                margin: EdgeInsets.symmetric(vertical: 5),
+                child: Card(
+                  child: ListTile(
+                    leading: Image.network(
+                        homeModel.article![index].urlToImage.toString()),
+                    title: Text(homeModel.article![index].title.toString()),
+                    subtitle: Text(
+                        homeModel.article![index].content!.substring(0, 50)),
+                  ),
+                ),
+              );
+            },
+          ),
+        ),
+      ],
     );
   }
 
--- lib/router.dart
+++ lib/router.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_newspaper_app/feature/home/data/homeuserdata.dart';
 
 import 'feature/home/presentation/screen/home_screen.dart';
 
@@ -7,8 +8,11 @@ class Routers {
     switch (settings.name) {
       case '/home-route':
         // final String? title = settings.arguments as String;
+        // final Homeduserdata? homeduserdata =
+        //     settings.arguments as Homeduserdata;
         return PageRouteBuilder<dynamic>(
-            pageBuilder: (_, __, ___) => HomeScreen("user"),
+            pageBuilder: (_, __, ___) =>
+                HomeScreen(homeduserdata: Homeduserdata(user: "user")),
             settings: RouteSettings(name: settings.name),
             transitionsBuilder: (_, Animation<double> a, __, Widget c) =>
                 FadeTransition(opacity: a, child: c));
